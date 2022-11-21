8a9615518a448b01fbe7031ecbdc7068b8e51853
manpreetsarao <111101038+manpreetsarao@users.noreply.github.com>
2022-11-20 18:36:14 -0800

    Deliverable 1

---
+++ MechaCarChallenge.R
@@ -0,0 +1,4 @@
+library(dplyr)
+demo_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=demo_table) #generate multiple linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=demo_table)) #generate summary statistics
