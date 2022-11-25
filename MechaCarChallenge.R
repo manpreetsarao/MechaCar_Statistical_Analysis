65fd3944c28b60c191e8dc832c01e321fcfa8d09
manpreetsarao <111101038+manpreetsarao@users.noreply.github.com>
2022-11-24 22:48:57 -0800

    T_test

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,16 @@
 library(dplyr)
 de_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=demo_table) #generate multiple linear regression model
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=demo_table)) #generate summary statistics
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=de_table) #generate multiple linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=de_table)) #generate summary statistics
 de_table2 <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 total_summary <- de_table2 %>%  summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
 lot_summary <- de_table2 %>% group_by(Manufacturing_Lot) %>%summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+# Perform a t-test on PSI across all manufacturing lots against the population mean of 1,500
+t.test(de_table2$PSI, mu=1500)
+
+# Perform a t-test on PSI for each manufacturing lot individually against the population mean of 1,500
+t.test(subset(de_table2, Manufacturing_Lot=='Lot1', PSI), mu=1500)
+t.test(subset(de_table2, Manufacturing_Lot=='Lot2', PSI), mu=1500)
+t.test(subset(de_table2, Manufacturing_Lot=='Lot3', PSI), mu=1500)
+
